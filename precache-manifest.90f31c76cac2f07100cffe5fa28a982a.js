e50b34a040617630473a00b19a9188ef0f52c427
BugaSeba <bugarinsebastian@gmail.com>
2023-03-02 22:16:42 +0100

    Updates

---
+++ precache-manifest.90f31c76cac2f07100cffe5fa28a982a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "658d9e81f83d31ca471b",
+    "url": "/static/css/main.245aebb5.chunk.css"
+  },
+  {
+    "revision": "658d9e81f83d31ca471b",
+    "url": "/static/js/main.658d9e81.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "d4686c376d25268d535a",
+    "url": "/static/js/2.d4686c37.chunk.js"
+  },
+  {
+    "revision": "fb573bd4075a37a7f80b",
+    "url": "/static/js/runtime~main.fb573bd4.js"
+  },
+  {
+    "revision": "b9004c789329957987b8283b4b3caf43",
+    "url": "/static/media/logo.b9004c78.jpg"
+  },
+  {
+    "revision": "03ebce603be35e93af566bbc4be36166",
+    "url": "/static/media/imagen8_Chalten.03ebce60.jpg"
+  },
+  {
+    "revision": "0aa7baeeb34012e1397b6dc9ae8de1fa",
+    "url": "/static/media/imagen9_Chalten.0aa7baee.jpg"
+  },
+  {
+    "revision": "1869171d077f3e988d840d84f31e3c49",
+    "url": "/static/media/imagen5_Chalten.1869171d.jpg"
+  },
+  {
+    "revision": "cb96ad2005898a956e6e283b28255630",
+    "url": "/static/media/imagen7_Chalten.cb96ad20.jpg"
+  },
+  {
+    "revision": "20bbe737ea2047100dcf631a89f71e7f",
+    "url": "/static/media/imagen3_Chalten.20bbe737.jpg"
+  },
+  {
+    "revision": "c2d5be91027f9de1ba5cdd839affc373",
+    "url": "/static/media/imagen6_Chalten.c2d5be91.jpg"
+  },
+  {
+    "revision": "21f0a67e571f7013e55d860642a3bc05",
+    "url": "/static/media/imagen4_Chalten.21f0a67e.jpg"
+  },
+  {
+    "revision": "1b513734d7b70831eaa76f660e5c7279",
+    "url": "/index.html"
+  }
+];
